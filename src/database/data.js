e866b790221f0bb95c3426e93e3b7276f1c79476
Fluxstride_boss <adekoyasamuel52@gmail.com>
2021-05-21 13:55:21 +0100

    -

--- src/database/data.js
+++ src/database/data.js
@@ -1,11 +1,4 @@
 let {MongoClient} = require("mongodb");
-//let dotenv = require("dotenv");
 let path = require("path")
-//dotenv.config({path: path.join(__dirname,".env")})
-let connectionString
-if(process.env.PORT){
-	    connectionString=process.env.MONGODB_URI;
-}else{
-	connectionString = process.env.MONGODB_OFF_URI;
-}
+let connectionString=process.env.MONGODB_URI;
 module.exports = new MongoClient(connectionString, { useUnifiedTopology: true });
